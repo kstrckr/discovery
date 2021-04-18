a2d8efe58074bf01d0e3d8b8ef872e19b8370300
Kurt Strecker <ksphotopro@gmail.com>
2021-04-18 17:19:01 -0400

    added published solution to compare

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -7,13 +7,25 @@ use aux5::{Delay, DelayMs, LedArray, OutputSwitch, entry};
 
 fn clean_tics(tic_ms: u16, leds: &mut LedArray, delay: &mut Delay) {
     for i in 0..8 {
-        let body = i;
+        // let body = i;
         let head = if i < 7 { i + 1 } else { 0 };
         let tail = if i > 0 { i - 1 } else { 7 };
         // leds[body].on().ok();
+        leds[tail].off().ok();
         delay.delay_ms(tic_ms);
         leds[head].on().ok();
-        leds[tail].off().ok();
+        delay.delay_ms(tic_ms);
+    }
+}
+
+fn pub_solution(ms: u16, leds: &mut LedArray, delay: &mut Delay) {
+    for curr in 0..8 {
+        let next = (curr + 1) % 8;
+
+        leds[next].on().ok();
+        delay.delay_ms(ms);
+        leds[curr].off().ok();
+        delay.delay_ms(ms);
     }
 }
 
@@ -23,6 +35,7 @@ fn main() -> ! {
     let tic = 50_u16;
     loop {
         clean_tics(tic, &mut leds, &mut delay);
+        // pub_solution(tic, &mut leds, &mut delay);
     }
 }
 
