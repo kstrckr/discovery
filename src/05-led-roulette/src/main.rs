44284b3356a7dcab73b25bca2bd88ab74504da65
Kurt Strecker <ksphotopro@gmail.com>
2021-04-18 01:35:36 -0400

    05 solution 1

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,36 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use volatile::Volatile;
+use aux5::{Delay, DelayMs, LedArray, OutputSwitch, entry};
+
+fn tic(leds: &mut LedArray, delay: &mut Delay) {
+
+    let interval: u16 = 50;
+    let overlap: u16 = 25;
+
+    for i in 0..8 {
+        leds[i].on().ok();
+        delay.delay_ms(interval);
+        if i == 7 {
+            leds[0].on().ok();
+        } else {
+            leds[i+1].on().ok();
+        }
+        delay.delay_ms(overlap);
+        leds[i].off().ok();
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let mut half_period = 500_u16;
+    let v_half_period = Volatile::new(&mut half_period);
+
+    loop {
+        tic(&mut leds, &mut delay);
+    }
 }
+
