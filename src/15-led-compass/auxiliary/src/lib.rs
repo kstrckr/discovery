c676eec07faadbf3a736ed828141ae5f58f392d5
Kurt Strecker <ksphotopro@gmail.com>
2021-05-02 03:05:00 -0400

    updated 15 to use the lsm303agr driver

--- src/15-led-compass/auxiliary/src/lib.rs
+++ src/15-led-compass/auxiliary/src/lib.rs
@@ -1,6 +1,6 @@
 //! Initialization code
-
 #![no_std]
+
 
 #[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust#53964
 extern crate panic_itm; // panic handler
@@ -8,17 +8,25 @@ extern crate panic_itm; // panic handler
 pub use cortex_m::{asm::bkpt, iprint, iprintln, peripheral::ITM};
 pub use cortex_m_rt::entry;
 pub use f3::{
-    hal::{delay::Delay, prelude, stm32f30x::i2c1},
+    hal::{delay::Delay, prelude, stm32f30x::{i2c1}},
     led::{Direction, Leds},
-    lsm303dlhc::I16x3,
 };
-
 use f3::{
-    hal::{i2c::I2c, prelude::*, stm32f30x},
-    Lsm303dlhc,
+    hal::{i2c::I2c, prelude::*, stm32f30x}
 };
 
-pub fn init() -> (Leds, Lsm303dlhc, Delay, ITM) {
+use f3::hal::gpio::gpioa::{PA5, PA6, PA7};
+use f3::hal::gpio::gpiob::{PB6, PB7};
+use f3::hal::gpio::gpioe::PE3;
+use f3::hal::gpio::{AF4, AF5, Output, PushPull};
+use f3::hal::spi::Spi;
+use f3::hal::stm32f30x::{I2C1, SPI1};
+
+
+pub use lsm303agr::{AccelOutputDataRate, Lsm303agr};
+// lsm303agr::interface::I2cInterface
+// I2c<I2C1, (PB6<AF4>, PB7<AF4>)
+pub fn init() -> (Leds, Delay, Lsm303agr<lsm303agr::interface::I2cInterface<I2c<I2C1, (PB6<AF4>, PB7<AF4>)>>, lsm303agr::mode::MagOneShot>, ITM) {
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = stm32f30x::Peripherals::take().unwrap();
 
@@ -36,9 +44,11 @@ pub fn init() -> (Leds, Lsm303dlhc, Delay, ITM) {
 
     let i2c = I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks, &mut rcc.apb1);
 
-    let lsm303dlhc = Lsm303dlhc::new(i2c).unwrap();
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
 
     let delay = Delay::new(cp.SYST, clocks);
 
-    (leds, lsm303dlhc, delay, cp.ITM)
+    (leds, delay, sensor, cp.ITM)
 }
--- src/15-led-compass/src/main.rs
+++ src/15-led-compass/src/main.rs
@@ -4,13 +4,22 @@
 
 #[allow(unused_imports)]
 use aux15::{entry, iprint, iprintln, prelude::*};
+pub use lsm303agr::{AccelOutputDataRate, Lsm303agr};
 
 #[entry]
 fn main() -> ! {
-    let (_leds, mut lsm303dlhc, mut delay, mut itm) = aux15::init();
+    let (_leds, mut delay, mut sensor, mut itm) = aux15::init();
+
 
     loop {
-        iprintln!(&mut itm.stim[0], "{:?}", lsm303dlhc.mag().unwrap());
-        delay.delay_ms(1_000_u16);
+        // iprintln!(&mut itm.stim[0], "{:?}", lsm303dlhc.mag().unwrap());
+        // delay.delay_ms(1_000_u16);
+        if sensor.accel_status().unwrap().xyz_new_data {
+            let data = sensor.accel_data().unwrap();
+
+            // format!("Acceleration: x {:?} y {:?} z {:?}", data.x, data.y, data.z);
+            // iprintln!("Acceleration: x {:?} y {:?} z {:?}", data.x, data.y, data.z);
+            iprintln!(&mut itm.stim[0], "{} - {} - {}", data.x, data.y, data.z);
+        }
     }
 }
